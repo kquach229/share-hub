e1d0a83f7e16aba7f3fe07d82ab104f5ac084dd3
Kenny Quach <kquach229@gmail.com>
2024-11-27 21:17:17 -0500

    save

--- app/api/quote/[id]/route.ts
+++ app/api/quote/[id]/route.ts
@@ -1,10 +1,7 @@
 import prisma from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
+export async function GET({ params }: { params: { id: string } }) {
   const { id } = params;
 
   try {
@@ -39,16 +36,13 @@ export async function GET(
   }
 }
 
-export async function DELETE(
-  request: NextRequest,
-  {
-    params,
-  }: {
-    params: {
-      id: string;
-    };
-  }
-) {
+export async function DELETE({
+  params,
+}: {
+  params: {
+    id: string;
+  };
+}) {
   const { id } = params;
 
   try {
