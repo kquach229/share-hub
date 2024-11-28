efe64c16fd1de7d4617b4c18e4e0e3e00223a7bb
Kenny Quach <kquach229@gmail.com>
2024-11-27 23:19:12 -0500

    save

--- app/api/quote/[id]/route.ts
+++ app/api/quote/[id]/route.ts
@@ -1,9 +1,6 @@
 import prisma from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
-import { NextRequest, NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
-
 export async function GET(
   request: NextRequest,
   { params }: { params: { id: string } }
